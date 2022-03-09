1b9be96d65d524309632eb86bf58d02227d73822
Alegre <leonardo.alegre.c@gmail.com>
2022-03-09 09:26:18 -0500

    .env agregado

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,20 @@
-console.log("hola mundo");
+import express, { Express } from 'express';
+import bodyParser from 'body-parser';
+import dotenv from 'dotenv';
+dotenv.config();
+
+const app: Express = express();
+
+app.use(bodyParser.json());
+
+app.use('*', (req, res) => {
+    return res.status(404).json({
+        success: false,
+        message: 'No hay nada por aquí',
+    })
+});
+
+const APP_PORT = process.env.APP_PORT ?? 3000;
+app.listen(APP_PORT, () => {
+    console.log('Escuchando en el puerto', APP_PORT);
+});
