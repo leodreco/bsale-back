22b44759dc4fe915164e63e267aa93c895761eac
Alegre <leonardo.alegre.c@gmail.com>
2022-03-10 07:50:00 -0500

    Agregando paginacion a producto

--- src/controllers/ProductController.ts
+++ src/controllers/ProductController.ts
@@ -4,12 +4,21 @@ import { Router, Request, Response } from 'express';
 const router = Router();
 
 router.get('/', async (req: Request, res: Response) => {
+    let skip = Number(req.query.skip ?? 0);
+    let take = Number(req.query.take ?? 10);
+
     const prisma = new PrismaClient();
-    const products = await prisma.product.findMany();
+    const products = await prisma.product.findMany({
+        skip,
+        take,
+    });
+    let count = await prisma.product.count();
+
     return res.json({
         success: true,
         data: products,
         message: null,
+        totalRows: count,
     });
 });
 
