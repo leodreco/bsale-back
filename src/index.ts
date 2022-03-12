44416a4cb7043fbceac21c5c591378739862044c
Alegre <leonardo.alegre.c@gmail.com>
2022-03-12 10:02:27 -0500

    Agregando process.env.PORT

--- src/index.ts
+++ src/index.ts
@@ -20,7 +20,7 @@ app.use('*', (req, res) => {
     })
 });
 
-const APP_PORT = process.env.APP_PORT ?? 3000;
+const APP_PORT = process.env.PORT ?? process.env.APP_PORT ?? 3000;
 app.listen(APP_PORT, () => {
     console.log('Escuchando en el puerto', APP_PORT);
 });
